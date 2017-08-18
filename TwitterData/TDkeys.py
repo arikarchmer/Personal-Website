653fa360cdca8bb3e7f8a35849aecd79848d2acf
Ari Karchmer <ari.karchmer.s@gmail.com>
2017-08-18 12:05:18 -0400

    Delete TDkeys.py

--- TwitterData/TDkeys.py
+++
@@ -1,8 +0,0 @@
-
-class TDkeys:
-
-    access_token = '725448716392476672-AuKWNnDbuOtnx5vd4yLRt4gvLxM8ERI'
-    access_token_secret = 'qaJPrCACNcQOdOab98VHkcWfpibXSXja8aqdtXH4H2w9o'
-    consumer_key = 'PIvNsRnnvyflWR3nIy5XRWwTk'
-    consumer_secret = 'xbMhxWLJ6hPtMaGot3BeboXQS3evHZVIw6ZIfdPVsnmHR7bECl'
-
